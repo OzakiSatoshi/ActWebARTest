385ee3cc76bfd5315d218c6aafba02a014e24fb4
OzakiSatoshi <60631984+OzakiSatoshi@users.noreply.github.com>
2021-05-25 17:04:50 +0900

    3

--- ModelChange.js
+++ ModelChange.js
@@ -1,5 +1,14 @@
 "use strict";
 
-let model = document.getElementById("model");
+let model = "";
+let donuts = '<a-entity id="donutsmodel" gltf-model="#donuts" scale="10 10 10" animation-mixer></a-entity>';
+
+console.log(donuts);
+
+function change(){
+  return model = donuts
+};
+
+window.addEventListener('click',change());
 
 console.log(model);
